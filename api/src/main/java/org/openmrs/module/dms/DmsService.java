cd9733d26dd2e6d4a9e28a72f6b928ddd73123e1
ghanshyamcusat2006 <ghanshyam.cusat2006@gmail.com>
2012-12-29 02:02:08 -0800

    ghanshyam date: 29-12-2012

--- api/src/main/java/org/openmrs/module/dms/DmsService.java
+++ api/src/main/java/org/openmrs/module/dms/DmsService.java
@@ -1,25 +1,30 @@
 /**
- * The contents of this file are subject to the OpenMRS Public License
- * Version 1.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://license.openmrs.org
+ *  Copyright 2012 Society for Health Information Systems Programmes, India (HISP India)
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
+ *  This file is part of Dms module.
  *
- * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
- */
+ *  Dms module is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  Dms module is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Dms module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
 package org.openmrs.module.dms;
 
-import java.text.ParseException;
 import java.util.List;
 
 import org.openmrs.Concept;
 import org.openmrs.ConceptAnswer;
 import org.openmrs.ConceptName;
-import org.openmrs.Patient;
 import org.openmrs.api.APIException;
 import org.openmrs.api.OpenmrsService;
 import org.openmrs.module.dms.model.DmsOpdUnit;
--- omod/src/main/java/org/openmrs/module/dms/web/controller/main/ActivateUnitController.java
+++ omod/src/main/java/org/openmrs/module/dms/web/controller/main/ActivateUnitController.java
@@ -1,3 +1,23 @@
+/**
+ *  Copyright 2012 Society for Health Information Systems Programmes, India (HISP India)
+ *
+ *  This file is part of Dms module.
+ *
+ *  Dms module is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  Dms module is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Dms module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
 package org.openmrs.module.dms.web.controller.main;
 
 import java.util.ArrayList;
